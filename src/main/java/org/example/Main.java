80792c1a6b78577cd2df4dfd5d4a44d036cfb123
olgacherry <zebroid81@yandex.ru>
2023-03-30 22:20:46 +0300

    дз исправлено

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String login = "java_skypro.go";
+        String login = "12345Java_skyprogo";
         String password = "D_1hWiKjjP_9";
         String confirmPassword = "D_1hWiKjjP_9";
 
--- src/main/java/org/example/checkLoginAndPassword.java
+++ src/main/java/org/example/checkLoginAndPassword.java
@@ -1,30 +1,36 @@
 package org.example;
 
+import java.util.regex.Pattern;
+
 public class checkLoginAndPassword {
 
     public static boolean check(String login, String password, String confirmPassword) {
 
         try {
-            if (login.length() > 20) {
+            if( login.length() > 20 || !Pattern.matches("^[a-zA-Z0-9_]$",login)){
                 throw new WrongLoginException(login);
             }
-            if (password.length() > 20) {
+            if( password.length() > 20 ||!Pattern.matches("^[a-zA-Z0-9_]$",password)) {
+
                 throw new WrongPasswordException(password);
+
+            }if (password.length() != confirmPassword.length()) {
+                    throw new WrongPasswordException(confirmPassword);
+
             }
-            if (password.length() != confirmPassword.length()) {
-                throw new WrongPasswordException(confirmPassword);
-            }
+
             return true;
         } catch (WrongLoginException exception) {
             System.out.println("Неверный формат логина");
         } catch (WrongPasswordException exception) {
             System.out.println("Неверный формат пароля");
             System.out.println("Пароли не совпадают");
-
         }
         return false;
     }
 }
+
+
 
 
 
